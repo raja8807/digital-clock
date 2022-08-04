0c2e631d7312c54d4fc8a1208c43e7fe51f546ff
Raja <rajarathinam1999@gmail.com>
2022-08-04 14:36:47 +0530

    completed

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ let monthOut = document.getElementById("month")
 let yearOut = document.getElementById("year")
 let dayOut = document.getElementById("day")
 
-let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-let days = ["Sun","Mon","Tue","Wed","Thurs","Fri","Sat"]
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
 
 let format = document.getElementById("format")
 
@@ -24,27 +24,26 @@ function renderTime() {
 
     minOut.innerText = minute;
 
-    if(format.value == "standard"){
+    if (format.value == "standard") {
         if (hour > 12) {
             hour = hour - 12;
             hourOut.innerText = hour;
             ampm.innerText = "PM"
-        }else if(hour == 00){
+        } else if (hour == 00) {
             hourOut.innerText = 12;
-            ampm.innerText = "AM"
         }
-        else{
+        else {
             hourOut.innerText = hour;
             ampm.innerText = "AM"
         }
-    }else{
+    } else {
         hourOut.innerText = hour;
         ampm.innerText = ""
     }
 
+
     if (second.toString().length === 2) {
         secOut.innerText = second;
-
     } else {
         secOut.innerText = "0" + second;
     }
@@ -54,7 +53,7 @@ function renderTime() {
     let year = time.getFullYear();
     let day = time.getDay();
     // console.log(day);
-    
+
     dateOut.innerText = date;
     monthOut.innerText = months[month];
     yearOut.innerText = year;
